986d98498da5de8065711ceaff5e1870617d7ad4
unknown <rlmclaughlin@yahoo.com>
2021-04-27 23:53:04 -0500

    update

--- knexfile.js
+++ knexfile.js
@@ -37,16 +37,7 @@ module.exports = {
 
     client: 'pg',
     useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 6,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false // <- default is true, set to false
-    },
+    pool: { min: 0, max: 30, acquireTimeoutMillis: 60 * 1000 },
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
