6fe1dd4be1405b6f09dbf3c6a38485b4a29848c6
unknown <rlmclaughlin@yahoo.com>
2021-04-27 21:26:11 -0500

    added pool

--- knexfile.js
+++ knexfile.js
@@ -34,6 +34,16 @@ module.exports = {
   },
 
   production: {
+    pool: {
+      "min": 2,
+      "max": 6,
+      "createTimeoutMillis": 3000,
+      "acquireTimeoutMillis": 30000,
+      "idleTimeoutMillis": 30000,
+      "reapIntervalMillis": 1000,
+      "createRetryIntervalMillis": 100,
+      "propagateCreateError": false // <- default is true, set to false
+    },
     client: 'pg',
     useNullAsDefault: true,
     connection: process.env.DATABASE_URL,
